f64930a8f991720922b4b8b84ea4110c9f60a9e2
Conartisttt <connerwmar@gmail.com>
2023-04-14 13:03:54 -0400

    no clue. added JS

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
-// When button is clicked, a series of prompts is presented
-
-
-
-
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 // Write password to the #password input
 function writePassword() {
-
-const passwordLength = prompt("How many characters would you like your password to be? Please choose between 8 and 128 characters.");
-
-const lowercaseCharacters = confirm("Do you want to include lowercase characters?");
-const uppercaseCharacters = confirm("Do you want to include uppercase characters?");
-const numericCharacters = confirm("Do you want to include numeric characters?");
-const specialCharacers = confirm("Do you want to include special characters?");
-
-
-// const  password = generatePassword();
-
-const passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
+const passwordLength = prompt("How many characters would you like your password to be? Please choose between 8 and 128 characters.");
+const includeLowercase = confirm("Do you want to include lowercase characters?");
+const includeUppercase = confirm("Do you want to include uppercase characters?");
+const includeNumeric = confirm("Do you want to include numeric characters?");
+const includeSpecial = confirm("Do you want to include special characters?");
 
+function generatePassword() {
 
-// The password can include special characters. 
-// If you’re unfamiliar with these, see this 
-// [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
+};
