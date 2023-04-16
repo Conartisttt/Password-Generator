30a46a71b0c169a75750023324e105bd4a2f1225
Conartisttt <connerwmar@gmail.com>
2023-04-16 18:46:47 -0400

    consolidated code & added css

--- script.js
+++ script.js
@@ -1,77 +1,78 @@
-// Original Assignment Code
-
-// Assignment Code
+// Assigns variable to button in HTML
 const generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  const passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
+// Adds event listener so that when the button is clicked, writePassword function occurs
 generateBtn.addEventListener("click", writePassword);
+
+// Writes the generated password to the #password input in HTML
+function writePassword() {
+    let password = generatePassword();
+    const passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
 
 // Arrays of options to generate password from
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const special = ["!", "$", "#", "%", "&", "(", ")", "*", ".", ",", "-", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "-", "_", "~", "|", "{", "}", "+"];
+const special = ['"', "'", "\\", "!", "$", "#", "%", "&", "(", ")", "*", ".", ",", "-", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "-", "_", "~", "|", "{", "}", "+"];
 
-// Create an array to store selected characters
-let includedCharacters = [];
-
-// Create a function that asks the users questions, and returns a password based on those answers
+// Asks the user a set of questions, and assigns their values to variables
 function generatePassword() {
-  const pwLength = parseInt(prompt("How many characters do you want your password to be? Please choose between 8 and 128."));
-  if (pwLength > 128) {
-    alert("You must choose a password between 8 and 128 characters");
-    return "";
-  }
-  if (pwLength < 8) {
-    alert("You must choose a password between 8 and 128 characters");
-    return "";
-  }
-  if (isNaN(pwLength)) {
-    alert("You must choose a password bewtween 8 and 128 characters");
-    return "";
-  }
 
-  const inLower = confirm("Do you want to include lowercase letters?");
-  if (inLower) {
-    includedCharacters = includedCharacters.concat(lowercase);
-  }
+    let password = "";
 
-  const inUpper = confirm("Do you want to include uppercase letters?");
-  if (inUpper) {
-    includedCharacters = includedCharacters.concat(uppercase);
-  }
+    const passwordLength = prompt("How many characters would you like your password to be? Please choose between 8 and 128 characters.");
 
-  const inNumeric = confirm("Do you want to include numbers?");
-  if (inNumeric) {
-    includedCharacters = includedCharacters.concat(numeric);
-  }
+    // Check if the user has entered a password length less than 8
+    if (passwordLength < 8) {
+        alert("You must select a password between 8 and 128 characters");
+        return passwordLength + " is less than the minimum allowable characters of 8.";
+    }
 
-  const inSpecial = confirm("Do you want to include special characters?");
-  if (inSpecial) {
-    includedCharacters = includedCharacters.concat(special);
-  }
+    // Check if the user has entered a password length greater than 128
+    if (passwordLength > 128) {
+        alert("You must select a password between 8 and 128");
+        return passwordLength + " is greater than the maximum allowable characters of 128.";
+    }
 
-  if (includedCharacters.length == 0) {
-    alert("You must choose at least one type of character");
-    return "";
-  }
+    // Check if the user is entering a number inside of passwordLength prompt, if not, return error message
+    if (isNaN(passwordLength)) {
+        alert("You must enter a number")
+        return passwordLength + " is not a number. Please enter a number between 8 and 128.";
+    }
 
-  let password = "";
+    const includeLowercase = confirm("Do you want to include lowercase characters?");
+    const includeUppercase = confirm("Do you want to include uppercase characters?");
+    const includeNumeric = confirm("Do you want to include numeric characters?");
+    const includeSpecial = confirm("Do you want to include special characters?");
 
-  for (let i = 0; i < pwLength; i++) {
-    const randomIndex = Math.floor(Math.random() * includedCharacters.length)
-    password += includedCharacters[randomIndex]
-  }
-  return password;
+    let selectedCharacters = [];
 
+    // Check which character types the user wants to include and add them to the selectedCharacters array
+    if (includeLowercase) {
+        selectedCharacters = selectedCharacters.concat(lowercase);
+    }
+    if (includeUppercase) {
+        selectedCharacters = selectedCharacters.concat(uppercase);
+    }
+    if (includeNumeric) {
+        selectedCharacters = selectedCharacters.concat(numeric);
+    }
+    if (includeSpecial) {
+        selectedCharacters = selectedCharacters.concat(special);
+    }
+
+    // Check if the user has selected at least one character type
+    if (selectedCharacters.length == 0) {
+        alert("You must select at least one character type.");
+        return "Without any character types selected, how am I supposed to generate a password? Please select at least one character type.";
+    }
+
+    // Generate a random password using the selected characters
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * selectedCharacters.length);
+        password += selectedCharacters[randomIndex];
+    }
+    return password;
 }
-
-
-
